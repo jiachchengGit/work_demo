cc135317eeab654f472a1eb85774c1126af835b2
chenjiacheng3 <cdchenjiacheng@jd.com>
2016-06-02 17:12:43 +0800

    add netty test code

---
+++ jccdemo-server/src/main/java/com/jd/jcc/server/netty/DiscardClient.java
@@ -0,0 +1,61 @@
+/**   
+* @Title: DiscardClient.java 
+* @Package com.jd.jcc.server.netty 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author chenjiacheng   
+* @date 2016年6月2日 下午3:25:15 
+* @version V1.0   
+*/
+package com.jd.jcc.server.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/** 
+ * @ClassName: DiscardClient 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author chenjiacheng
+ * @date 2016年6月2日 下午3:25:15 
+ *  
+ */
+public class DiscardClient {
+
+	/**
+	 * @throws InterruptedException  
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @Author chenjiacheng
+	 * @Date 2016年6月2日 下午3:25:15
+	 * @param args        
+	 * @throws 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		 String host = "127.0.0.1";
+	        int port = 8080;
+	        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+	        try {
+	            Bootstrap b = new Bootstrap(); // (1)
+	            b.group(workerGroup); // (2)
+	            b.channel(NioSocketChannel.class); // (3)
+	            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+	            b.handler(new ChannelInitializer<SocketChannel>() {
+	                @Override
+	                public void initChannel(SocketChannel ch) throws Exception {
+	                    ch.pipeline().addLast(new DiscardClientHandler(),new DiscardClientHandler2());
+	                }
+	            });
+
+	            ChannelFuture f = b.connect(host, port).sync(); // (5)
+	            f.channel().closeFuture().sync();
+	        } finally {
+	            workerGroup.shutdownGracefully();
+	        }
+	}
+
+}
---
+++ jccdemo-server/src/main/java/com/jd/jcc/server/netty/DiscardServerHandler.java
@@ -0,0 +1,56 @@
+/**   
+* @Title: DiscardServerHandler.java 
+* @Package com.jd.jcc.server.netty 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author chenjiacheng   
+* @date 2016年6月2日 下午2:45:35 
+* @version V1.0   
+*/
+package com.jd.jcc.server.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/** 
+ * @ClassName: DiscardServerHandler 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author chenjiacheng
+ * @date 2016年6月2日 下午2:45:35 
+ *  
+ */
+public class DiscardServerHandler  extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+    	   ByteBuf in = (ByteBuf) msg;
+    	    try {
+    	        while (in.isReadable()) { // (1)
+    	            System.out.print((char) in.readByte());
+    	            System.out.flush();
+    	        }
+    	        System.out.println();
+    	    } finally {
+    	        ReferenceCountUtil.release(msg); // (2)
+    	    }
+    }
+    
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ChannelFuture writeAndFlush = ctx.writeAndFlush(getSendMsg());
+	}
+
+	private ByteBuf getSendMsg(){
+    	ByteBuf bb = Unpooled.buffer();
+    	bb.writeBytes("this is server send msg".getBytes());
+    	return bb;
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
